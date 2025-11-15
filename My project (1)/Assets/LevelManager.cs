1e779a20a0c7b25814505b3bd9898bbd6de71d77
oGgame <70044124+saahas123@users.noreply.github.com>
2025-11-15 17:28:51 -0500

    resync

--- My project (1)/Assets/LevelManager.cs
+++ My project (1)/Assets/LevelManager.cs
@@ -1,46 +1,54 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class LevelMangaer : MonoBehaviour
+public class LevelManager : MonoBehaviour
 {
-    public GameObject levelPrefab;    // Your level prefab
-    public int initialPieces = 3;     // Number of pieces to spawn at start
-    public float levelLength = 135f;  // Length of your prefab along Z
+    [Header("Level Prefabs")]
+    public List<GameObject> levelPrefabs; // Add all your prefabs here
+    public int initialPieces = 3;         // Pieces spawned at start
 
     private List<GameObject> activeLevels = new List<GameObject>();
     private Transform player;
 
     void Start()
     {
-        // Find the player
         player = GameObject.FindWithTag("Player").transform;
 
         // Spawn initial pieces
         for (int i = 0; i < initialPieces; i++)
         {
-            SpawnLevel(i);
+            SpawnNextLevel();
         }
     }
 
     void Update()
     {
-        // If player is approaching the end of the last piece, spawn a new one
-        GameObject lastLevel = activeLevels[activeLevels.Count - 1];
-        float lastLevelEndZ = lastLevel.transform.position.z + (levelLength / 2f);
+        if (activeLevels.Count == 0) return;
 
-        if (player.position.z > lastLevelEndZ - (levelLength * 1.5f))
+        GameObject lastLevel = activeLevels[activeLevels.Count - 1];
+        float lastLevelEndZ = lastLevel.transform.position.z + GetPrefabLength(lastLevel) / 2f;
+
+        float spawnBuffer = 20f; // How far ahead to spawn
+        if (player.position.z > lastLevelEndZ - spawnBuffer)
         {
-            SpawnLevel(activeLevels.Count);
+            SpawnNextLevel();
             DestroyOldestLevel();
         }
     }
 
-    void SpawnLevel(int index)
+    void SpawnNextLevel()
     {
-        // Center pivot adjustment
-        float spawnZ = index * levelLength;
-        Vector3 spawnPos = new Vector3(0, 0, spawnZ);
-        GameObject newLevel = Instantiate(levelPrefab, spawnPos, Quaternion.identity);
+        // Pick a random prefab (repeats allowed)
+        GameObject prefab = levelPrefabs[Random.Range(0, levelPrefabs.Count)];
+
+        float spawnZ = 0f;
+        if (activeLevels.Count > 0)
+        {
+            GameObject lastLevel = activeLevels[activeLevels.Count - 1];
+            spawnZ = lastLevel.transform.position.z + GetPrefabLength(lastLevel);
+        }
+
+        GameObject newLevel = Instantiate(prefab, new Vector3(0, 0, spawnZ), Quaternion.identity);
         activeLevels.Add(newLevel);
     }
 
@@ -51,5 +59,17 @@ public class LevelMangaer : MonoBehaviour
             Destroy(activeLevels[0]);
             activeLevels.RemoveAt(0);
         }
+    }
+
+    float GetPrefabLength(GameObject level)
+    {
+        Collider col = level.GetComponent<Collider>();
+        if (col != null) return col.bounds.size.z;
+
+        Renderer rend = level.GetComponentInChildren<Renderer>();
+        if (rend != null) return rend.bounds.size.z;
+
+        Debug.LogWarning("Prefab has no collider or renderer! Defaulting to 135 units.");
+        return 135f;
     }
 }
