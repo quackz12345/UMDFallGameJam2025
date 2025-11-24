cee3613d2fe6440200e0177a38660060ab340272
oGgame <70044124+saahas123@users.noreply.github.com>
2025-11-23 21:21:26 -0500

    uhkuh

---
+++ My project (1)/Assets/SceneLoader.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game"); // replace "Game" with your scene name
+        Debug.Log("pressed");
+    }
+}
